434a45b164acb805f9cea3515ed5930f7332d059
Abhishek Mishra <abhishekmishra3@gmail.com>
2024-02-26 23:10:02 +0530

    added initial shader test program

---
+++ love2d-pixelshader-first/conf.lua
@@ -0,0 +1,22 @@
+--- conf.lua: Config for the love2d game.
+--
+-- date: 26/2/2024
+-- author: Abhishek Mishra
+
+-- canvas size
+local canvasWidth = 400
+local canvasHeight = 400
+
+function love.conf(t)
+    -- set the window title
+    t.window.title = "Simple love2d shader example"
+
+    -- set the window size
+    t.window.width = canvasWidth
+    t.window.height = canvasHeight
+
+    -- disable unused modules for performance
+    t.modules.joystick = false
+    t.modules.physics = false
+    t.modules.touch = false
+end
---
+++ love2d-pixelshader-first/main.lua
@@ -0,0 +1,82 @@
+--- main.lua: A simple love2d shader example with some basic shaders.
+--
+-- date: 26/2/2024
+-- author: Abhishek Mishra
+
+local function createShaderProgram(programName, shaderCode)
+    local shader = love.graphics.newShader(shaderCode)
+    local program = {
+        name = programName,
+        shader = shader
+    }
+    return program
+end
+
+local identityShader = [[
+    vec4 effect( vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords ) {
+      vec4 pixel = Texel(texture, texture_coords );//This is the current pixel color
+      return pixel * color;
+    }
+]]
+
+local grayscaleShader = [[
+    vec4 effect( vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords ) {
+      vec4 pixel = Texel(texture, texture_coords );//This is the current pixel color
+      number gray = (color.r + color.g + color.b) / 3.0;
+      return pixel * vec4(gray, gray, gray, 1.0);
+    }
+]]
+
+local programs = {
+    createShaderProgram("Identity", identityShader),
+    createShaderProgram("Grayscale", grayscaleShader)
+}
+
+local currentId
+local currentProgram
+local cw, ch
+
+function love.load()
+    currentId = 1
+    currentProgram = programs[currentId]
+    cw, ch = love.graphics.getWidth(), love.graphics.getHeight()
+end
+
+function love.update()
+
+end
+
+function love.draw()
+    love.graphics.setShader(currentProgram.shader)
+
+    --draw something here
+    love.graphics.setColor(0.1, 0.15, 0.2, 1)
+    love.graphics.rectangle("fill", 100, 100, 200, 100)
+    love.graphics.setColor(1, 0, 0, 1)
+    love.graphics.rectangle("fill", 100, 200, 200, 100)
+
+    love.graphics.setShader()
+
+    --draw the name of the current shader at the top left of the screen
+    love.graphics.setColor(0, 1, 1, 1)
+    love.graphics.print('Shader: ' .. currentProgram.name, 10, 10)
+
+    -- show fps
+    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.print('FPS: ' .. love.timer.getFPS(),
+        cw - 50, ch - 20)
+end
+
+--- escape to exit
+function love.keypressed(key)
+    if key == "escape" then
+        love.event.quit()
+    end
+    if key == "space" or key == "enter" then
+        currentId = currentId + 1
+        if currentId > #programs then
+            currentId = 1
+        end
+        currentProgram = programs[currentId]
+    end
+end
