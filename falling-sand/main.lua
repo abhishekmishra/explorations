ab93e1f5cf4c80b62122bd9e8e7a6599e3681354
Abhishek Mishra <abhishekmishra3@gmail.com>
2024-02-26 18:08:12 +0530

    added hsv colour support so that sand can be multi-coloured, with the hue changing over time. Added doc about how the sim works

--- falling-sand/main.lua
+++ falling-sand/main.lua
@@ -2,6 +2,15 @@
 -- This is heavily based on the video by The Coding Train Coding Challenge #180:
 -- Falling Sand at https://www.youtube.com/watch?v=L4u7Zy_b868
 --
+-- Here's how this works:
+-- * When the user clicks and drags the mouse, the cells under the mouse are
+--  filled with sand.
+-- * The sand starts falling down.
+-- * If the cell below is empty, the sand falls down.
+-- * If the cell below is filled, the sand falls to the left or right
+--  with a 50% probability.
+-- * If there is no space to fall, the sand stays in place.
+--
 -- date: 26/2/2024
 -- author: Abhishek Mishra
 
@@ -13,6 +22,9 @@ local grid, nextGrid, gridRows, gridCols
 
 -- dragging flag
 local dragging = false
+
+-- colour hue value
+local hue = 0
 
 -- seed the random number generator
 math.randomseed(os.time())
@@ -69,13 +81,11 @@ function love.update()
                     and mouseCol + x > 0 and mouseRow + x > 0 then
                     -- fill the cell with 75% probability
                     if math.random() > 0.25 then
-                        nextGrid[mouseCol + x][mouseRow + y] = 1
+                        nextGrid[mouseCol + x][mouseRow + y] = hue
                     end
                 end
             end
         end
-
-        nextGrid[mouseCol][mouseRow] = 1
     end
 
     -- Loop through the grid
@@ -84,7 +94,7 @@ function love.update()
             local state = grid[x][y]
 
             -- If the cell is filled
-            if state == 1 then
+            if state > 0 then
                 local below = grid[x][y + 1]
 
                 local belowA, belowB
@@ -107,16 +117,16 @@ function love.update()
                 -- If the cell below is empty
                 if below == 0 then
                     -- Move the cell down
-                    nextGrid[x][y + 1] = 1
+                    nextGrid[x][y + 1] = state
                     nextGrid[x][y] = 0
                 elseif belowA == 0 then
                     -- Move the cell down and to the A direction
-                    nextGrid[x + direction][y + 1] = 1
+                    nextGrid[x + direction][y + 1] = state
                 elseif belowB == 0 then
                     -- Move the cell down and to the B direction
-                    nextGrid[x - direction][y + 1] = 1
+                    nextGrid[x - direction][y + 1] = state
                 else
-                    nextGrid[x][y] = 1
+                    nextGrid[x][y] = state
                 end
             end
         end
@@ -124,6 +134,9 @@ function love.update()
 
     -- Update the grid
     grid = nextGrid
+
+    -- increment the hue value
+    hue = (hue + 1) % 360
 end
 
 --- love.draw: Called every frame, draws the simulation
@@ -134,9 +147,9 @@ function love.draw()
     -- Draw the grid
     for x = 1, gridRows do
         for y = 1, gridCols do
-            if grid[x][y] == 1 then
+            if grid[x][y] > 0 then
                 -- Set color to purple for filled cells
-                love.graphics.setColor(128, 0, 128)
+                love.graphics.setColor(HSV(grid[x][y] / 360, 1, 1))
                 -- Draw a 1x1 rectangle for each cell
                 love.graphics.rectangle("fill", (x - 1) * gridW,
                     (y - 1) * gridW, gridW, gridW)
@@ -172,4 +185,28 @@ function love.keypressed(key)
     if key == "escape" then
         love.event.quit()
     end
+end
+
+--- Converts HSV to RGB. (input and output range: 0 - 1)
+-- see https://love2d.org/wiki/HSV_color
+function HSV(h, s, v)
+    if s <= 0 then return v,v,v end
+    h = h*6
+    local c = v*s
+    local x = (1-math.abs((h%2)-1))*c
+    local m,r,g,b = (v-c), 0, 0, 0
+    if h < 1 then
+        r, g, b = c, x, 0
+    elseif h < 2 then
+        r, g, b = x, c, 0
+    elseif h < 3 then
+        r, g, b = 0, c, x
+    elseif h < 4 then
+        r, g, b = 0, x, c
+    elseif h < 5 then
+        r, g, b = x, 0, c
+    else
+        r, g, b = c, 0, x
+    end
+    return r+m, g+m, b+m
 end
