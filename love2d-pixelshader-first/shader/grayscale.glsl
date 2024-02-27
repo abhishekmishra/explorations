83b2cf42f902976674bc1ac44e836aafe471a5f8
Abhishek Mishra <abhishekmishra3@gmail.com>
2024-02-27 11:20:32 +0530

    converted existing shaders to glsl3

--- love2d-pixelshader-first/shader/grayscale.glsl
+++ love2d-pixelshader-first/shader/grayscale.glsl
@@ -1,6 +1,15 @@
-vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) {
+#pragma language glsl3
+
+//NOTE: "texture" is reserved in GLSL3, so we use "texture_image" instead
+
+vec4 effect(
+    vec4 color,
+    Image texture_image,
+    vec2 texture_coords,
+    vec2 screen_coords
+) {
     //This is the current pixel color
-    vec4 pixel = Texel(texture, texture_coords);
+    vec4 pixel = Texel(texture_image, texture_coords);
     number gray = (color.r + color.g + color.b) / 3.0;
     return pixel * vec4(gray, gray, gray, 1.0);
 }
