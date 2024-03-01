fa61e5f057528df3ae52405475bcae671c01ed2d
Abhishek Mishra <abhishekmishra3@gmail.com>
2024-03-01 23:38:44 +0530

    clamp position to boundary if the ball exceeds it.

--- metaballs/main.lua
+++ metaballs/main.lua
@@ -16,7 +16,7 @@ function Metaball:initialize(x, y, r, v)
     self.r = r
 
     -- velocity
-    self.v = v or {x = 200, y = 50}
+    self.v = v or { x = 200, y = 50 }
 end
 
 --- Metaball:draw: Draw the metaball
@@ -36,10 +36,6 @@ function Metaball:update(dt)
     local cw = love.graphics.getWidth()
     local ch = love.graphics.getHeight()
 
-    -- update the position
-    self.x = self.x + self.v.x * dt
-    self.y = self.y + self.v.y * dt
-
     -- bounce off the walls
     if (self.x - self.r) < 0 or (self.x + self.r) > cw then
         self.v.x = -(self.v.x)
@@ -47,6 +43,23 @@ function Metaball:update(dt)
     if (self.y - self.r) < 0 or (self.y + self.r) > ch then
         self.v.y = -(self.v.y)
     end
+
+    -- check if position is beyond the canvas then clamp it
+    if self.x < self.r then
+        self.x = self.r
+    elseif self.x > cw - self.r then
+        self.x = cw - self.r
+    end
+
+    if self.y < self.r then
+        self.y = self.r
+    elseif self.y > ch - self.r then
+        self.y = ch - self.r
+    end
+
+    -- update the position
+    self.x = self.x + self.v.x * dt
+    self.y = self.y + self.v.y * dt
 end
 
 local balls
@@ -60,7 +73,7 @@ function love.load()
         local r = love.math.random(2, 50)
         local x = love.math.random(r, love.graphics.getWidth() - r)
         local y = love.math.random(r, love.graphics.getHeight() - r)
-        local v = {x = love.math.random(-200, 200), y = love.math.random(-200, 200)}
+        local v = { x = love.math.random(-200, 200), y = love.math.random(-200, 200) }
         local b = Metaball:new(x, y, r, v)
         table.insert(balls, b)
     end
@@ -94,11 +107,11 @@ end
 --- Converts HSV to RGB. (input and output range: 0 - 1)
 -- see https://love2d.org/wiki/HSV_color
 function HSV(h, s, v)
-    if s <= 0 then return v,v,v end
-    h = h*6
-    local c = v*s
-    local x = (1-math.abs((h%2)-1))*c
-    local m,r,g,b = (v-c), 0, 0, 0
+    if s <= 0 then return v, v, v end
+    h = h * 6
+    local c = v * s
+    local x = (1 - math.abs((h % 2) - 1)) * c
+    local m, r, g, b = (v - c), 0, 0, 0
     if h < 1 then
         r, g, b = c, x, 0
     elseif h < 2 then
@@ -112,5 +125,5 @@ function HSV(h, s, v)
     else
         r, g, b = c, 0, x
     end
-    return r+m, g+m, b+m
+    return r + m, g + m, b + m
 end
