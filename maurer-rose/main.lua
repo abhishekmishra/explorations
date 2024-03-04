fe89404ffd646334e909914ab3ecbdc6f45d94bf
Abhishek Mishra <abhishekmishra3@gmail.com>
2024-03-04 22:58:55 +0530

    initial maurer rose sim project

---
+++ maurer-rose/main.lua
@@ -0,0 +1,22 @@
+--- main.lua: A Maurer Rose Simulation in LÖVE
+-- date: 4/3/2024
+-- author: Abhishek Mishra
+
+--- love.load: Called once at the start of the simulation
+function love.load()
+end
+
+--- love.update: Called every frame, updates the simulation
+function love.update(dt)
+end
+
+--- love.draw: Called every frame, draws the simulation
+function love.draw()
+end
+
+-- escape to exit
+function love.keypressed(key)
+    if key == "escape" then
+        love.event.quit()
+    end
+end
