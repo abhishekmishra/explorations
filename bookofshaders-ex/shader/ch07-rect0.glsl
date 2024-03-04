dc079ba5cbaa6a310fd75fddae4698edc9429005
Abhishek Mishra <abhishekmishra3@gmail.com>
2024-03-04 15:47:38 +0530

    naming and comments

--- bookofshaders-ex/shader/ch07-rect0.glsl
+++ bookofshaders-ex/shader/ch07-rect0.glsl
@@ -22,10 +22,9 @@ vec4 effect(
 
     vec3 pixelColor = vec3(0.0);
 
-    vec2 bl = step(vec2(0.1),st);       // bottom-left
-    vec2 tr = step(vec2(0.1),1.0-st);   // top-right
-    pixelColor = vec3(bl.x * bl.y * tr.x * tr.y);
+    vec2 tl = step(vec2(0.1),st);       // top-left
+    vec2 br = step(vec2(0.1),1.0-st);   // bottom-right
+    pixelColor = vec3(tl.x * tl.y * br.x * br.y);
 
-    // return a purple colour for all pixels.
     return vec4(pixelColor, 1.0);
 }
