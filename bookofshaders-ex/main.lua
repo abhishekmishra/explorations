74f6e4f02be2b466dd242cb6976db3060bb45c7f
Abhishek Mishra <abhishekmishra3@gmail.com>
2024-03-24 10:58:55 +0530

    distance scaled and then discretized to create a circle

--- bookofshaders-ex/main.lua
+++ bookofshaders-ex/main.lua
@@ -70,8 +70,10 @@ local programs = {
     --     "shader/ch07-rect3-util-fns.glsl"),
     -- shaderProg("Ch 7: Distance from center",
     --    "shader/ch07-dist0.glsl"),
-    shaderProg("Ch 7: Distance from center scaled/normalized",
-        "shader/ch07-dist1-fullgradient.glsl"),
+    -- shaderProg("Ch 7: Distance from center scaled/normalized",
+    --     "shader/ch07-dist1-fullgradient.glsl"),
+    shaderProg("Ch 7: Distance scaled and used to create circle",
+        "shader/ch07-dist2-gradientstep.glsl"),
 }
 
 local shaderId
---
+++ bookofshaders-ex/shader/ch07-dist2-gradientstep.glsl
@@ -0,0 +1,39 @@
+/**
+* file: ch07-dist2-gradientstep.glsl: We take the distance from the center of
+* the screen. Then we use this distance with a step function to create a sharp
+* edge which defines a circle. All pixels inside the circle are black and all
+* pixels outside the circle are white.
+*
+* date: 10/03/2024
+* author: Abhishek Mishra
+*/
+#pragma language glsl3
+
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+vec4 effect(
+    vec4 color,
+    Image tex,
+    vec2 texture_coords,
+    vec2 screen_coords
+)
+{
+    //Normalize the Screen coordinates. 
+    vec2 st = screen_coords.xy / love_ScreenSize.xy;
+    float pct = 0.0;
+
+    // The DISTANCE from the pixel to the center
+    pct = distance(st, vec2(0.5));
+
+    // scale to [0, 1]
+    pct = pct * 2.0;
+
+    // Use the step function to create a sharp edge
+    pct = step(0.5, pct);
+
+    vec3 pixelColor = vec3(pct);
+
+    return vec4(pixelColor, 1.0);
+}
