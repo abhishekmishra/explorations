1e3a44f02aabaa9e5fe6a45e78ff774cf70036bf
Abhishek Mishra <abhishekmishra3@gmail.com>
2024-03-30 14:25:06 +0530

    Initial QuadTree class

---
+++ quadtree-sim/quadtree.lua
@@ -0,0 +1,14 @@
+--- quadtree.lua: QuadTree Class
+-- date: 30/3/2024
+-- author: Abhishek Mishra
+
+local Class = require('middleclass')
+
+-- QuadTree Class
+local QuadTree = Class('QuadTree')
+
+-- Constructor
+function QuadTree:initialize()
+end
+
+return QuadTree
