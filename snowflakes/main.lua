2893e8e0c3b87dd79b592d2eb6c88e834222f867
Abhishek Mishra <abhishekmishra3@gmail.com>
2024-04-09 18:28:09 +0530

    reset function to be able to reset the simulation at any time.

--- snowflakes/main.lua
+++ snowflakes/main.lua
@@ -8,10 +8,13 @@ local SnowFlake = require 'snowflake'
 local snowflakes
 
 -- font for credits
-local creditsFont
+local creditsFont = love.graphics.newFont(12)
+
+-- background music
+local music = love.audio.newSource("A Lucid Dream.ogg", "stream")
 
 -- number of snowflakes to create
-local NUM_SNOWFLAKES = 3000
+local NUM_SNOWFLAKES = 5000
 
 --- createSnowflake: Create a new snowflake
 -- @return SnowFlake: A new snowflake object
@@ -19,24 +22,28 @@ local function createSnowflake()
     return SnowFlake:new(
         math.random(0, love.graphics.getWidth()),
         math.random(-love.graphics.getHeight(),
-            love.graphics.getHeight() / 10),
+            love.graphics.getHeight() * 0.05),
         math.random(1, 3))
 end
 
---- love.load: Called once at the start of the simulation
-function love.load()
-    -- create the snowflakes for the simulation
+--- restart: Restart the simulation
+local function restart()
     snowflakes = {}
     for i = 1, NUM_SNOWFLAKES do
         table.insert(snowflakes, createSnowflake())
     end
 
-    creditsFont = love.graphics.newFont(12)
+    -- if there is already music playing, stop it
+    love.audio.stop()
 
     -- load music and play
-    local music = love.audio.newSource("A Lucid Dream.ogg", "stream")
     music:setLooping(true)
     love.audio.play(music)
+end
+
+--- love.load: Called once at the start of the simulation
+function love.load()
+    restart()
 end
 
 --- love.update: Called every frame, updates the simulation
@@ -54,10 +61,10 @@ function love.draw()
     end
 
     -- draw text at the bottom of the screen to show credits
-    love.graphics.setColor(0.7, 0.5, 0.5)
+    love.graphics.setColor(0.7, 0.5, 0.5, 0.8)
     -- set credits font
     love.graphics.setFont(creditsFont)
-    love.graphics.print("Snowflakes Simulation in LÖVE by ne0l4t3r4l", 10, love.graphics.getHeight() - 50)
+    love.graphics.print("Snowflakes Simulation in LÖVE by ne0l4t3r4l (https://neolateral.in)", 10, love.graphics.getHeight() - 50)
     love.graphics.print("Pixel-art snowflakes by https://opengameart.org/users/alxl", 10, love.graphics.getHeight() - 35)
     love.graphics.print("Background Score - \"The Lucid Dream\" by https://opengameart.org/users/caliderium", 10, love.graphics.getHeight() - 20)
 
@@ -70,5 +77,8 @@ end
 function love.keypressed(key)
     if key == "escape" then
         love.event.quit()
+    end
+    if key == 'r' then
+        restart()
     end
 end
