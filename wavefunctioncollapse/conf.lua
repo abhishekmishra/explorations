71d96661492cc379e112ced5e189127136bdd0f0
Abhishek Mishra <abhishekmishra3@gmail.com>
2024-04-30 19:07:40 +0530

    created new project for Wave Function Collapse

---
+++ wavefunctioncollapse/conf.lua
@@ -0,0 +1,27 @@
+--- conf.lua: Config for the love2d game.
+--
+-- date: 30/04/2024
+-- author: Abhishek Mishra
+
+-- canvas size
+local canvasWidth = 400
+local canvasHeight = 400
+
+function love.conf(t)
+    -- set the window title
+    t.window.title = "Wave Function Collapse Simulation"
+
+    -- set the window size
+    t.window.width = canvasWidth
+    t.window.height = canvasHeight
+
+    -- disable unused modules for performance
+    t.modules.joystick = false
+    t.modules.physics = false
+    t.modules.touch = false
+
+    -- enable console
+    -- TODO: turning on console crashes Love2D on Windows,
+    -- so it's disabled for now
+    -- t.console = true
+end
---
+++ wavefunctioncollapse/main.lua
@@ -0,0 +1,22 @@
+--- main.lua: Wave Function Collapse Simulation in LÖVE
+-- date: 30/04/2024
+-- author: Abhishek Mishra
+
+--- love.load: Called once at the start of the simulation
+function love.load()
+end
+
+--- love.update: Called every frame, updates the simulation
+function love.update(dt)
+end
+
+--- love.draw: Called every frame, draws the simulation
+function love.draw()
+end
+
+-- escape to exit
+function love.keypressed(key)
+    if key == "escape" then
+        love.event.quit()
+    end
+end
