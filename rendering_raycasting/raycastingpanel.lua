e80e5b1909432a47cffbfa8fb9ae0605ee415599
Abhishek Mishra <abhishekmishra3@gmail.com>
2024-05-01 23:32:06 +0530

    moved the raycasting simulation setup to separate class

--- rendering_raycasting/raycastingpanel.lua
+++ rendering_raycasting/raycastingpanel.lua
@@ -9,6 +9,7 @@ local Class = require('middleclass')
 local nl = require('ne0luv')
 local Boundary = require('boundary')
 local Particle = require('particle')
+local RaycastingSystem = require('raycastingsystem')
 
 --- A Raycasting panel class
 local RaycastingPanel = Class('Raycasting', nl.Panel)
@@ -18,71 +19,30 @@ function RaycastingPanel:initialize(x, y, w, h)
     -- call the parent class constructor
     nl.Panel.initialize(self, nl.Rect(x, y, w, h))
 
-    -- walls
-    self:createWalls()
-
-    -- particle
-    self.particle = Particle(nl.Vector(self:getWidth() / 2,
-        self:getHeight() / 2))
-
-    -- particle offsets to move with noise
-    self.xOffset = 0
-    self.yOffset = 10000
-end
-
-function RaycastingPanel:createWalls()
-    local cw = self:getWidth()
-    local ch = self:getHeight()
-
-    -- initialize the walls
-    self.walls = {}
-
-    -- create some random boundaries
-    for _ = 1, 5 do
-        local x1 = math.random(0, cw)
-        local y1 = math.random(0, ch)
-        local x2 = math.random(0, cw)
-        local y2 = math.random(0, ch)
-        table.insert(self.walls, Boundary(x1, y1, x2, y2))
-    end
-
-    -- add walls which form a box the size of the canvas
-    table.insert(self.walls, Boundary(0, 0, cw, 0))
-    table.insert(self.walls, Boundary(cw, 0,
-        cw, ch))
-    table.insert(self.walls, Boundary(cw, ch, 0, ch))
-    table.insert(self.walls, Boundary(0, ch, 0, 0))
+    -- create the raycasting system
+    self.raycastingSystem = RaycastingSystem(self:getWidth(), self:getHeight()) 
 end
 
 -- Panel update
 function RaycastingPanel:update(dt)
-    -- move the particle with noise
-    local x = love.math.noise(self.xOffset) * self:getWidth()
-    local y = love.math.noise(self.yOffset) * self:getHeight()
-    self.particle:move(x, y)
-
-    -- update the offsets
-    self.xOffset = self.xOffset + 0.01
-    self.yOffset = self.yOffset + 0.01
-
-    -- cast the rays from the particle
-    self.particle:look(self.walls)
+    -- update the raycasting system
+    self.raycastingSystem:update(dt)
 end
 
 --- Override the draw method to draw the rays
 function RaycastingPanel:draw()
     -- draw the boundaries
-    for _, wall in ipairs(self.walls) do
+    for _, wall in ipairs(self.raycastingSystem.walls) do
         wall:draw()
     end
 
     -- draw the particle
-    self.particle:draw()
+    self.raycastingSystem.particle:draw()
 end
 
 function RaycastingPanel:keypressed(key)
     if key == "r" then
-        self:createWalls()
+        self.raycastingSystem:createWalls()
     end
 end
 
---
+++ rendering_raycasting/raycastingsystem.lua
@@ -0,0 +1,66 @@
+--- raycastingsystem.lua - The Raycasting System for the raycasting simulation.
+--
+-- date: 1/5/2024
+-- author: Abhishek Mishra
+
+local Class = require('middleclass')
+
+local nl = require('ne0luv')
+local Boundary = require('boundary')
+local Particle = require('particle')
+
+local RaycastingSystem = Class('RaycastingSystem')
+
+--- Constructor for the Raycasting System
+function RaycastingSystem:initialize(w, h)
+    self.cw = w
+    self.ch = h
+
+    -- walls
+    self:createWalls()
+
+    -- particle
+    self.particle = Particle(nl.Vector(self.cw / 2,
+        self.ch / 2))
+
+    -- particle offsets to move with noise
+    self.xOffset = 0
+    self.yOffset = 10000
+end
+
+function RaycastingSystem:createWalls()
+    -- initialize the walls
+    self.walls = {}
+
+    -- create some random boundaries
+    for _ = 1, 5 do
+        local x1 = math.random(0, self.cw)
+        local y1 = math.random(0, self.ch)
+        local x2 = math.random(0, self.cw)
+        local y2 = math.random(0, self.ch)
+        table.insert(self.walls, Boundary(x1, y1, x2, y2))
+    end
+
+    -- add walls which form a box the size of the canvas
+    table.insert(self.walls, Boundary(0, 0, self.cw, 0))
+    table.insert(self.walls, Boundary(self.cw, 0,
+        self.cw, self.ch))
+    table.insert(self.walls, Boundary(self.cw, self.ch, 0, self.ch))
+    table.insert(self.walls, Boundary(0, self.ch, 0, 0))
+end
+
+function RaycastingSystem:update(dt)
+    -- move the particle with noise
+    local x = love.math.noise(self.xOffset) * self.cw
+    local y = love.math.noise(self.yOffset) * self.ch
+    self.particle:move(x, y)
+
+    -- update the offsets
+    self.xOffset = self.xOffset + 0.01
+    self.yOffset = self.yOffset + 0.01
+
+    -- cast the rays from the particle
+    self.particle:look(self.walls)
+end
+
+return RaycastingSystem
