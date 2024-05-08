5d1f442cc37e25d6cd04ce85600707e271de5d6b
Abhishek Mishra <abhishekmishra3@gmail.com>
2024-05-08 18:32:49 +0530

    start with just a single circle drawn as a polygon

--- polar-perlin-noise-loop/main.lua
+++ polar-perlin-noise-loop/main.lua
@@ -4,10 +4,15 @@
 
 -- All imports and module scope variables go here.
 
+-- canvas dimensions
+local cw, ch
+
+
 
 --- love.load: Called once at the start of the simulation
 ---@diagnostic disable-next-line: duplicate-set-field
 function love.load()
+    cw, ch = love.graphics.getDimensions()
 end
 
 
@@ -22,6 +27,27 @@ end
 --- love.draw: Called every frame, draws the simulation
 ---@diagnostic disable-next-line: duplicate-set-field
 function love.draw()
+    -- draw a circle by drawing line segments
+    -- at every segment turn by a small angle
+    -- before drawing the next one.
+    -- use this to draw a circle like a polygon
+    -- using the love.graphics.polygon function
+    love.graphics.push()
+    love.graphics.translate(cw/2, ch/2)
+    local radius = 100
+    local angle_delta = 0.1
+    local segments = 2 * math.pi / angle_delta
+    -- generate the vertices for the circle
+    local vertices = {}
+    for i = 1, segments do
+        local x = radius * math.cos(i * angle_delta)
+        local y = radius * math.sin(i * angle_delta)
+        table.insert(vertices, x)
+        table.insert(vertices, y)
+    end
+    -- draw the circle
+    love.graphics.polygon("line", vertices)
+    love.graphics.pop()
 end
 
 
