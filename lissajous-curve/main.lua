91646c09867de01500eb0913ae0a6e66b73e4736
Abhishek Mishra <abhishekmishra3@gmail.com>
2024-05-30 22:29:43 +0530

    basic sine curve

--- lissajous-curve/main.lua
+++ lissajous-curve/main.lua
@@ -21,6 +21,18 @@ end
 
 
 function love.draw()
+    -- draw a sine curve
+    local x0, y0 = 0, ch/2
+    local x1, y1 = cw, ch/2
+    love.graphics.line(x0, y0, x1, y1)
+    local amp = 100
+    local freq = 1
+    local phase = 0
+    local y = 0
+    for x = 0, cw do
+        y = y0 + amp * math.sin(2 * math.pi * freq * x / cw + phase)
+        love.graphics.points(x, y)
+    end
 end
 
 
