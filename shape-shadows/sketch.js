2753e49e6d9693dc0645e5ecb8e97a0d8a10a711
Abhishek Mishra <abhishekmishra3@gmail.com>
2026-03-09 10:44:16 +0530

    ellipse and rectangle

--- shape-shadows/sketch.js
+++ shape-shadows/sketch.js
@@ -22,6 +22,8 @@ function draw() {
   let tx = (sin(frameCount * 0.1) + 1) / 2;
   drawSquare(20, 20, 80, tx);
   drawCircle(180, 60, 80, tx);
+  drawEllipse(300, 60, 90, 60, tx);
+  drawRectangle(380, 30, 110, 60, tx);
 }
 
 function drawSquare(x, y, w, tx) {
@@ -62,6 +64,52 @@ function drawCircle(x, y, r, tx) {
 
   fill(COLOR_DARKEST);
   circle(x + dx, y + dy, r);
+
+  pop();
+}
+
+function drawEllipse(x, y, w, h, tx) {
+  push();
+  //location of shadow
+  let sx = x + 20;
+  let sy = y + 20;
+  let sw = w * 0.9;
+  let sh = h * 0.9;
+
+  let dx = tx * w * 0.2;
+  let dy = tx * h * 0.2;
+  let dw = tx * w * 0.1;
+  let dh = tx * w * 0.1;
+
+  noStroke();
+  fill(COLOR_DARK);
+  ellipse(sx + dx / 2, sy + dy / 2, sw + dw, sh + dh);
+
+  fill(COLOR_DARKEST);
+  ellipse(x + dx, y + dy, w, h);
+
+  pop();
+}
+
+function drawRectangle(x, y, w, h, tx) {
+  push();
+  //location of shadow
+  let sx = x + 20;
+  let sy = y + 20;
+  let sw = w * 0.9;
+  let sh = h * 0.9;
+
+  let dx = tx * w * 0.2;
+  let dy = tx * h * 0.2;
+  let dw = tx * w * 0.1;
+  let dh = tx * w * 0.1;
+
+  noStroke();
+  fill(COLOR_DARK);
+  rect(sx + dx / 2, sy + dy / 2, sw + dw, sh + dh);
+
+  fill(COLOR_DARKEST);
+  rect(x + dx, y + dy, w, h);
 
   pop();
 }
