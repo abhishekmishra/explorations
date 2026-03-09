28f5ceab8f0a53eba19e06bf99a274b31e592b37
Abhishek Mishra <abhishekmishra3@gmail.com>
2026-03-09 09:53:55 +0530

    initial p5js project template

---
+++ p5-empty-project/sketch.js
@@ -0,0 +1,14 @@
+let CANVAS_WIDTH = 640;
+let CANVAS_HEIGHT = 480;
+
+function setup() {
+  let cnv = createCanvas(
+    CANVAS_WIDTH,
+    CANVAS_HEIGHT,
+    document.getElementById("exp-canvas"),
+  );
+}
+
+function draw() {
+  background(220);
+}
