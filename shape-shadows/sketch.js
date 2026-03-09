c0b99799369e5ece4cc3750ac30f5792a808371d
Abhishek Mishra <abhishekmishra3@gmail.com>
2026-03-09 10:35:43 +0530

    circle with shadow

--- shape-shadows/sketch.js
+++ shape-shadows/sketch.js
@@ -19,25 +19,49 @@ function setup() {
 function draw() {
   background(COLOR_LIGHT);
 
-  drawSquare(100, 100, 100);
+  let tx = (sin(frameCount * 0.1) + 1) / 2;
+  drawSquare(20, 20, 80, tx);
+  drawCircle(180, 60, 80, tx);
 }
 
-function drawSquare(x, y, w) {
+function drawSquare(x, y, w, tx) {
   push();
   //location of shadow
-  let sx = x * 1.3;
-  let sy = y * 1.3;
-  let sw = w * 0.85;
+  let sx = x + 20;
+  let sy = y + 20;
+  let sw = w * 0.9;
 
-  let dx = (sin(frameCount * 0.1) + 1) * 10;
-  let dy = (sin(frameCount * 0.1) + 1) * 10;
-  let dw = (sin(frameCount * 0.1) + 1) * 10;
+  let dx = tx * w * 0.2;
+  let dy = tx * w * 0.2;
+  let dw = tx * w * 0.1;
 
+  noStroke();
   fill(COLOR_DARK);
   square(sx + dx / 2, sy + dy / 2, sw + dw);
 
   fill(COLOR_DARKEST);
   square(x + dx, y + dy, w);
+
+  pop();
+}
+
+function drawCircle(x, y, r, tx) {
+  push();
+  //location of shadow
+  let sx = x + 20;
+  let sy = y + 20;
+  let sw = r * 0.9;
+
+  let dx = tx * r * 0.2;
+  let dy = tx * r * 0.2;
+  let dw = tx * r * 0.1;
+
+  noStroke();
+  fill(COLOR_DARK);
+  circle(sx + dx / 2, sy + dy / 2, sw + dw);
+
+  fill(COLOR_DARKEST);
+  circle(x + dx, y + dy, r);
 
   pop();
 }
