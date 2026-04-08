43b7f60ff0627fbe900742d76187e5cbe4624243
Abhishek Mishra <abhishekmishra3@gmail.com>
2026-04-08 22:22:58 +0530

    initial poperties of vehicle

--- auto-vehicles/vehicle.lua
+++ auto-vehicles/vehicle.lua
@@ -1,9 +1,15 @@
 local Class = require 'middleclass'
+local Vector = require 'vector'
 
 local Vehicle = Class('Vehicle')
 
 function Vehicle:initialize()
-
+    self.mass = 0
+    self.position = Vector(0, 0)
+    self.velocity = Vector(0, 0)
+    self.maxForce = 0
+    self.maxSpeed = 0
+    self.orientation = 0
 end
 
 function Vehicle:draw()
